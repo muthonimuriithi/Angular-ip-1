3dda3c6f2e0e60f56c6060f562541abeb2c8436e
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-10 21:21:27 +0300

    imported the class quotes to it

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -10,7 +10,9 @@ export class FirstComponent implements OnInit {
 
   quotes: Quotes[] = [
     new Quotes(1, `First Quote`, 0, 0),
-    new Quotes(1, `First Quote`, 0, 0),
+    new Quotes(2, `Second Quote`, 0, 0),
+    new Quotes(3, `Third Quote`, 0, 0),
+
 
   ]
 
