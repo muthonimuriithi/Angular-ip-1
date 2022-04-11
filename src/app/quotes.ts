6d8232680d8b2b9772d22af96abac628708e2248
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-11 08:10:26 +0300

    modified id element

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -7,7 +7,7 @@ export class Quotes {
     downvotes: number
 
     constructor (id:number, quote:string, upvotes:number, downvotes:number) {
-    this.id = id;
+    this.id = id
     this.quote = quote
     this.upvotes = upvotes
     this.downvotes = downvotes
