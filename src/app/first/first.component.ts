931f4a2bfe5191ab051ad1a3dcba3a74ffdee31f
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-11 07:51:18 +0300

    added quotes and their authors

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../quotes';
 export class FirstComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes(1, `First Quote`, 0, 0),
-    new Quotes(2, `Second Quote`, 0, 0),
-    new Quotes(3, `Third Quote`, 0, 0),
+    new Quotes(1, `Kindness always comes back. By: k-cloe`, 0, 0),
+    new Quotes(2, `Never regret anything that made you smile. By: l-Herman`, 0, 0),
+    new Quotes(3, `Worry less, smile more. By: T.june`, 0, 0),
 
 
   ]
